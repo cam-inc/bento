bffb060a53740f336d348094b51b1140a4ccb0ee
Go Ohtani <cathcheeno@gmail.com>
2022-06-23 16:03:54 +0900

    chore(@bento-editor/website): deploy to gh-pages

--- workspaces/website/docusaurus.config.js
+++ workspaces/website/docusaurus.config.js
@@ -8,16 +8,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://cam-inc.github.io',
+  baseUrl: '/bento/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cam-inc', // Usually your GitHub org/user name.
+  projectName: 'bento', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
