940062d9f1ff52cf240dddba378cd78da4a62053
Go Ohtani <cathcheeno@gmail.com>
2022-07-12 16:26:08 +0900

    chore(@bento-editor/example-simple): introduce Vite as a build tool

--- examples/simple/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Greeter, Editor } from '@bento-editor/core';
-
-const aa = Greeter('hello');
-console.log(aa);
-
-const editor = new Editor();
-console.log(editor);
---
+++ examples/simple/src/main.ts
@@ -0,0 +1,12 @@
+import { Editor } from '@bento-editor/core';
+import './style.css';
+
+const canvasElm = document.querySelector<HTMLDivElement>('#canvas');
+
+if (!canvasElm) {
+  throw new Error('Could not find the element#canvas.');
+}
+
+const editor = new Editor({
+  holder: canvasElm
+});
