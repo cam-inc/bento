2bdf61b463588fbd6396580c98fe53cb8bf560b4
Go Ohtani <cathcheeno@gmail.com>
2022-07-21 14:32:45 +0900

    chore(@bento-editor/tool-block-paragraph): renderSettings

--- examples/simple/src/main.ts
+++ examples/simple/src/main.ts
@@ -4,17 +4,19 @@ import Editor, { LogLevels } from '@editorjs/editorjs';
 import './style.css';
 
 const canvasElm = document.querySelector<HTMLDivElement>('#canvas');
+const saveElm = document.querySelector<HTMLButtonElement>('#save');
 
 if (!canvasElm) {
   throw new Error('Could not find the element#canvas.');
 }
 
-new Editor({
+const editor = new Editor({
   holder: canvasElm,
   tools: {
     myparagraph: {
       // TODO: fix type error
       class: ToolBlockParagraph,
+      inlineToolbar: true,
     }
   },
   data: {
@@ -45,4 +47,12 @@ new Editor({
   autofocus: true,
   placeholder: 'Let`s write an awesome story!',
   //logLevel: LogLevels.VERBOSE,
+});
+
+saveElm?.addEventListener('click', () => {
+  editor.save().then(outputData => {
+    console.log('saved: ', outputData);
+  }).catch(error => {
+    console.log('save failed: ', error);
+  })
 });
