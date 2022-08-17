10e3bae81c775c5f3cecedb67dce3bc580cca0cd
Go Ohtani <cathcheeno@gmail.com>
2022-08-17 16:30:03 +0900

    refactor(@bento-editor/core): lodash to just

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,5 +1,5 @@
-import _ from 'lodash';
+import justDebounceIt from 'just-debounce-it';
 
 export const isBrower: boolean = typeof window !== 'undefined';
 
-export const debounce = _.debounce;
+export const debounce = justDebounceIt;
