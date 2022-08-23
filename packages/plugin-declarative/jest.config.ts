ec7a1bef932bd71b7959ece7d521e9669f3f5447
shuta13 <hollystarsun@gmail.com>
2022-08-23 13:15:59 +0900

    chore: delete plugin-declarative

--- packages/plugin-declarative/jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { InitialOptionsTsJest } from 'ts-jest';
-
-const config: InitialOptionsTsJest = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-};
-
-export default config;
--- packages/plugin-declarative/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export { createElement as h, Fragment } from '../src/create-element';
-export * from '../src/create-plugin';
-export * from '../src/hooks';
-export * from '../src/types';
