85cb61267217e9f9a4904795796f138e5fbef46e
shuta13 <hollystarsun@gmail.com>
2022-10-04 12:20:23 +0900

    feat: add skelton

--- packages/text-format/src/toolbar/index.css.ts
+++ packages/text-format/src/toolbar/index.css.ts
@@ -1,5 +1,5 @@
 import { style } from '@vanilla-extract/css';
-import { /*themeVars,*/ EditorClassName } from '@bento-editor/core';
+import { themeVars, EditorClassName } from '@bento-editor/core';
 
 export const styles = {
   list: style({
@@ -15,6 +15,58 @@ export const styles = {
         display: 'block',
         width: 20,
         height: 20,
+      },
+    },
+  }),
+  buttonBox: style({
+    selectors: {
+      [`.${EditorClassName} &`]: {
+        display: 'flex',
+        placeItems: 'center',
+        fontSize: themeVars.fontSize.label.medium,
+        whiteSpace: 'nowrap',
+      },
+    },
+  }),
+  featureIcon: style({
+    selectors: {
+      [`.${EditorClassName} &`]: {
+        display: 'block',
+        width: 20,
+        height: 20,
+        color: themeVars.color.brand,
+        marginRight: themeVars.space[1],
+      },
+    },
+  }),
+  dropDownIcon: style({
+    selectors: {
+      [`.${EditorClassName} &`]: {
+        display: 'block',
+        width: 20,
+        height: 20,
+        color: themeVars.color.backgroundOnSlight,
+        marginLeft: themeVars.space[1],
+      },
+    },
+  }),
+  moreButton: style({
+    selectors: {
+      [`.${EditorClassName} &`]: {
+        display: 'block',
+        width: 20,
+        height: 20,
+        color: themeVars.color.backgroundOnSlight,
+      },
+    },
+  }),
+  verticalRuler: style({
+    selectors: {
+      [`.${EditorClassName} &`]: {
+        width: '1px',
+        backgroundColor: themeVars.color.backgroundOnFaint,
+        height: '100%',
+        margin: `0 ${themeVars.space[2]}`,
       },
     },
   }),
