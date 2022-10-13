cc37a7928c832614d398a5b30316f86cac12a8de
Go Ohtani <cathcheeno@gmail.com>
2022-10-13 13:59:42 +0900

    refactor: optional props

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -99,7 +99,6 @@ const Home: NextPage = () => {
     }),
     []
   );
-
   const initialValue = useMemo<EditorProps['initialValue']>(
     () => [
       {
@@ -449,9 +448,9 @@ const Home: NextPage = () => {
   return (
     <div>
       <Editor
-        config={config}
-        initialValue={initialValue}
-        onChange={handleChange}
+      //        config={config}
+      //        initialValue={initialValue}
+      //        onChange={handleChange}
       />
       <EditorRenderer renderers={renderers} data={value} config={config} />
     </div>
