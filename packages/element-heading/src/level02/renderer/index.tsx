c644723e09bc137eae9b6f232c9d53aa7f3e622f
shuta13 <hollystarsun@gmail.com>
2022-10-14 15:47:53 +0900

    refactor: remove `import React from 'react'` from element-heading

--- packages/element-heading/src/level02/renderer/index.tsx
+++ packages/element-heading/src/level02/renderer/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { RendererProps } from '@bento-editor/core';
 import { Attributes } from '../attributes';
 
 export const Level02Renderer: React.FC<RendererProps<Attributes>> = ({
   children,
 }) => {
-  return <h2>{children}</h2>
+  return <h2>{children}</h2>;
 };
--- packages/element-heading/src/level03/renderer/index.tsx
+++ packages/element-heading/src/level03/renderer/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { RendererProps } from '@bento-editor/core';
 import { Attributes } from '../attributes';
 
 export const Level03Renderer: React.FC<RendererProps<Attributes>> = ({
   children,
 }) => {
-  return <h3>{children}</h3>
+  return <h3>{children}</h3>;
 };
