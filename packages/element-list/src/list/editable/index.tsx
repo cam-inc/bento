ab4c7be60646d249dc46d22ced252a951d28dc07
shuta13 <hollystarsun@gmail.com>
2022-10-14 16:06:35 +0900

    refactor: remove `import React from 'react'` from element-list

--- packages/element-list/src/list/editable/index.tsx
+++ packages/element-list/src/list/editable/index.tsx
@@ -1,5 +1,4 @@
 import { Element, ElementContainer, helpers } from '@bento-editor/core';
-import React from 'react';
 import { Attributes } from '../attributes';
 import { styles } from './index.css';
 
