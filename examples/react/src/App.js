cc85a4a7aa69fedf1a41427f3e81863c933755a8
Go Ohtani <cathcheeno@gmail.com>
2022-10-24 13:15:22 +0900

    refactor: react example

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -119,8 +119,8 @@ export default function App() {
   };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello</h1>
+      <h2>Start editing to try out!</h2>
       <Editor
         config={config}
         initialValue={initialValue}
