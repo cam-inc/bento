78fad55c4c4f9f1b96d4b37df03d767d108bae63
hiteto218 <hidemaruhidekyun@gmail.com>
2023-03-22 11:14:17 +0900

    fix: unused helpers 🐛

--- packages/core/src/helpers/attributes/index.ts
+++
@@ -1,6 +0,0 @@
-import { Editor } from 'slate';
-
-export const isActiveAttribute = (editor: Editor, key: string) => {
-  const marks = Editor.marks(editor);
-  return !!marks?.attributes?.[key];
-};
