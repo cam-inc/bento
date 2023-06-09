6c0ee44184bcaade67afba979c481a6bbf851c2f
MasanobuRyuman <22011376mr@tama.ac.jp>
2023-06-09 19:45:57 +0900

    feat: made the deleteRange method return void :sparkles:

--- packages/core/src/helpers/range/index.ts
+++ packages/core/src/helpers/range/index.ts
@@ -1,12 +1,8 @@
-import { Editor, Point, Range } from 'slate';
+import { Editor, Range } from 'slate';
 
-export const deleteRange = (editor: Editor, range: Range): Point | null => {
+export const deleteRange = (editor: Editor, range: Range) => {
   if (Range.isCollapsed(range)) {
-    return range.anchor;
-  } else {
-    const [, end] = Range.edges(range);
-    const pointRef = Editor.pointRef(editor, end);
-    editor.delete({ at: range });
-    return pointRef.unref();
+    return;
   }
+  editor.delete({ at: range });
 };
