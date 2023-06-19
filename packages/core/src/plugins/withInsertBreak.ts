13b8e5fa89887484de3959ffe42117daf3f2a2c4
MasanobuRyuman <22011376mr@tama.ac.jp>
2023-06-19 19:11:20 +0900

    refactor: refactor withInsertBreak

--- packages/core/src/plugins/withInsertBreak.ts
+++ packages/core/src/plugins/withInsertBreak.ts
@@ -1,78 +1,93 @@
-import { Element, Range, Text } from 'slate';
+import { Element, Node, Range, Text } from 'slate';
 import { Editor } from 'slate';
 import { EditorPlugin } from '.';
 import { helpers } from '../helpers';
 
 /**
- * If the original insertBreak is set, execute it.
- * If enter is pressed at the beginning of a line, insert the default Element at the line above.
- * If enter is pressed in the line, it will wrap the line and overwrite the default Element.
- * If enter is pressed at the end of a line, insert the default Element at the below.
- * If the string is empty, overwrite that line with the default Element.
+ * case1: original insertBreak is set.
+ * action: execute original insertBreak.
+ *
+ * case2: enter is pressed at the beginning of a line.
+ * action: insert the default Element at the line above.
+ *
+ * case3: enter is pressed in the line.
+ * action: it will wrap the line and overwrite the default Element.
+ *
+ * case4: enter is pressed at the end of a line.
+ * action: insert the default Element at the below.
+ *
+ * case5: string is empty.
+ * action: overwrite that line with the default Element.
  */
 export const withInsertBreak: EditorPlugin = (editor, config) => {
-  const originalInsertBreak = editor.insertBreak;
-
   // @see: https://docs.slatejs.org/api/nodes/editor#insertbreak-greater-than-void
   editor.insertBreak = () => {
     const { selection } = editor;
-    if (selection) {
-      helpers.rangeHelpers.deleteRange(editor, selection);
-      const [match] = Editor.nodes(editor, {
-        match: (n) => !Editor.isEditor(n) && Element.isElement(n),
-        mode: 'lowest',
-      });
-
-      if (match) {
-        const [node] = match;
-        if (Element.isElement(node)) {
-          const { elements } = config;
-          const element = elements.find((element) => {
-            return element.type === node.type;
-          });
-          if (element && element.insertBreak) {
-            const isToReturn = element.insertBreak(editor, match, config);
-            if (isToReturn) {
-              return;
-            }
-          }
-        }
-      }
-
-      const currentNode = editor.children[selection.anchor.path[0]];
-      if (!Element.isElement(currentNode)) {
-        return;
-      }
-      const pointStart = Range.start(selection);
-      const textList = Array.from(
-        editor.nodes({
-          at: [selection.anchor.path[0]],
-          match: (node) => Text.isText(node),
-        })
-      ).map((node) => (Text.isText(node[0]) ? node[0].text : ''));
-      const isEmptyText = !textList.join();
-      if (isEmptyText && currentNode.type !== config.defaultElement.type) {
-        editor.setNodes({
-          type: config.defaultElement.type,
-        });
-        return;
-      }
-      editor.splitNodes({ always: true });
-      if (pointStart.path[1] === 0 && pointStart.offset === 0) {
-        editor.setNodes(
-          {
-            type: config.defaultElement.type,
-          },
-          { at: [selection.anchor.path[0]] }
-        );
-      } else {
-        editor.setNodes({
-          type: config.defaultElement.type,
-        });
-      }
+    if (!selection) {
       return;
     }
-    originalInsertBreak();
+
+    helpers.rangeHelpers.deleteRange(editor, selection);
+
+    const [match] = editor.nodes({
+      match: (n) => !Editor.isEditor(n) && Element.isElement(n),
+      mode: 'lowest',
+    });
+    if (!match) {
+      return;
+    }
+
+    const [currentNode] = match;
+    if (!Element.isElement(currentNode)) {
+      return;
+    }
+
+    // when there is an original insertBreak
+    const { elements } = config;
+    const element = elements.find((element) => {
+      return element.type === currentNode.type;
+    });
+    if (element && element.insertBreak) {
+      const isToReturn = element.insertBreak(editor, match, config);
+      if (isToReturn) {
+        return;
+      }
+    }
+
+    // when text is empty
+    const textEntryList = Array.from(Node.texts(currentNode));
+    const [firstTextNode] = textEntryList[0];
+    if (!Text.isText(firstTextNode)) {
+      return;
+    }
+    if (
+      !firstTextNode.text.length &&
+      currentNode.type !== config.defaultElement.type
+    ) {
+      editor.setNodes({
+        type: config.defaultElement.type,
+      });
+      return;
+    }
+
+    editor.splitNodes({ always: true });
+    // When the cursor is at the beginning of the line
+    const pointStart = Range.start(selection);
+    const isFirst = pointStart.path[1] === 0 && pointStart.offset === 0;
+    if (isFirst) {
+      editor.setNodes(
+        {
+          type: config.defaultElement.type,
+        },
+        { at: [selection.anchor.path[0]] }
+      );
+      return;
+    }
+
+    editor.setNodes({
+      type: config.defaultElement.type,
+    });
+    return;
   };
   return editor;
 };
