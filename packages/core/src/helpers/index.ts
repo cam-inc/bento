2b7c992a5bd320fea12d65f87b053ef716ffa140
MasanobuRyuman <22011376mr@tama.ac.jp>
2023-06-22 17:25:19 +0900

    fix: fixed error when deleting nested elements :bug:

--- packages/core/src/helpers/index.ts
+++ packages/core/src/helpers/index.ts
@@ -1,13 +1,14 @@
 import { style } from '@vanilla-extract/css';
+import * as childHelpers from './child';
 import Element, * as elements from './element';
-import * as logger from './logger';
 import * as error from './error';
+import * as logger from './logger';
 import Node from './node';
+import * as normalizeNode from './normlizeNode';
 import Path, * as pathHelpers from './path';
 import ReactEditor from './reactEditor';
-import * as transforms from './transform';
 import Text, * as textHelpers from './text';
-import * as childHelpers from './child';
+import * as transforms from './transform';
 
 export const helpers = {
   ReactEditor,
@@ -15,6 +16,7 @@ export const helpers = {
   logger,
   error,
   Node,
+  normalizeNode,
   Path,
   ...transforms,
   Text,
