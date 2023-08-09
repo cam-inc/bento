0ce0d6f7ee671b8319fc634e00a0b177cffa8a28
nonoakij <nonoakij@gmail.com>
2023-08-09 21:06:01 +0900

    test(using pacakages/jest-presets): init

---
+++ packages/jest-presets/jest-preset.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/test/__fixtures__',
+    '<rootDir>/node_modules',
+    '<rootDir>/dist',
+  ],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
